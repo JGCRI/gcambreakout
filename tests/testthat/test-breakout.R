004a2e8a43a6c1fd07faa6a73c11c024198d7b37
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-02-05 12:32:15 -0500

    Updating tests

--- tests/testthat/test-breakout.R
+++ tests/testthat/test-breakout.R
@@ -4,8 +4,8 @@ library(gcambreakout); library(testthat)
 test_that("breakout generates new files", {
 
   # Reset data test data
-  gcamdataFolderOriginal = paste(getwd(),"/gcamdata_test_Original",sep="")
-  gcamdataFolder = paste(getwd(),"/gcamdata_test",sep="")
+  gcamdataFolderOriginal = paste(getwd(),"/original",sep="")
+  gcamdataFolder = paste(getwd(),"/test",sep="")
   unlink(gcamdataFolder,recursive=T)
   dir.create(gcamdataFolder)
   file.copy(paste(gcamdataFolderOriginal,"/inst",sep=""), gcamdataFolder, recursive=TRUE)
@@ -15,18 +15,18 @@ test_that("breakout generates new files", {
                           countriesNew = c("Thailand","Lao Peoples Democratic Republic"))
 
   expect_true(all(
-    file.exists(paste(getwd(),"/gcamdata_test/inst/extdata/common/iso_GCAM_regID_Original.csv",sep = "")),
-    file.exists(paste(getwd(),"/gcamdata_test/inst/extdata/common/iso_GCAM_regID_Original.csv",sep = "")),
-    file.exists(paste(getwd(),"/gcamdata_test/inst/extdata/common/GCAM_region_names_Original.csv",sep = "")),
-    file.exists(paste(getwd(),"/gcamdata_test/inst/extdata/aglu/A_bio_frac_prod_R_Original.csv",sep = "")),
-    file.exists(paste(getwd(),"/gcamdata_test/inst/extdata/aglu/A_soil_time_scale_R_Original.csv",sep = "")),
-    file.exists(paste(getwd(),"/gcamdata_test/inst/extdata/emissions/A_regions_Original.csv",sep = "")),
-    file.exists(paste(getwd(),"/gcamdata_test/inst/extdata/energy/A23.subsector_interp_R_Original.csv",sep = "")),
-    file.exists(paste(getwd(),"/gcamdata_test/inst/extdata/energy/A_regions_Original.csv",sep = "")),
-    file.exists(paste(getwd(),"/gcamdata_test/inst/extdata/energy/offshore_wind_potential_scaler_Original.csv",sep = ""))
+    file.exists(paste(getwd(),"/test/inst/extdata/common/iso_GCAM_regID_Original.csv",sep = "")),
+    file.exists(paste(getwd(),"/test/inst/extdata/common/iso_GCAM_regID_Original.csv",sep = "")),
+    file.exists(paste(getwd(),"/test/inst/extdata/common/GCAM_region_names_Original.csv",sep = "")),
+    file.exists(paste(getwd(),"/test/inst/extdata/aglu/A_bio_frac_prod_R_Original.csv",sep = "")),
+    file.exists(paste(getwd(),"/test/inst/extdata/aglu/A_soil_time_scale_R_Original.csv",sep = "")),
+    file.exists(paste(getwd(),"/test/inst/extdata/emissions/A_regions_Original.csv",sep = "")),
+    file.exists(paste(getwd(),"/test/inst/extdata/energy/A23.subsector_interp_R_Original.csv",sep = "")),
+    file.exists(paste(getwd(),"/test/inst/extdata/energy/A_regions_Original.csv",sep = "")),
+    file.exists(paste(getwd(),"/test/inst/extdata/energy/offshore_wind_potential_scaler_Original.csv",sep = ""))
     )
     )
 
-  gcambreakout::restore(gcamdataFolder = paste(getwd(),"/gcamdata_test",sep=""))
+  gcambreakout::restore(gcamdataFolder = paste(getwd(),"/test",sep=""))
 
 })
